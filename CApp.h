03c3cd7e4a9a2c3fd2e127b3107287dff293d96e
Zshandi Krahn <ZshandiK@gmail.com>
2020-03-20 10:26:38 -0700

    Document CApp functions
    I added comments explaining what the CApp functions do. In the process, I figured out a couple things I need to change.

--- CApp.h
+++ CApp.h
@@ -35,22 +35,44 @@ private:
 
     bool render = true;
 public:
+    // Starts a new game
     void newGame();
+    // Checks the current state for win conditions
     void checkWin();
+    // Gets the next turn (player 1 or player 2) based on the current turn
     int switchTurn(int turn);
 
+    // Undoes the last move (If playing against computer, last 2 moves
     void undo();
+    // Redoes the last move that was undone
     void redo();
+    // Clears history to prevent "redoing" after making a moved
+    //  (doesn't actually check anything, I need to rename it)
     void checkHistory();
 
+    // Very confusing(should be 3 functions):
+    // 1. If i is null(0) and x and y are not:
+    //      Sets values pointed by x and y to current mouse position
+    // 2. If x or y are null and i is not:
+    //      Sets value pointed by i to grid index of current mouse position
+    // 3. If none of i, x, or y are null:
+    //      Sets values pointed by x and y to be coordinates of grid space
+    //       with index of value pointed by i
+    // 4. Otherwise does nothing
     void getPosition(int* i, int* x = 0, int* y = 0);
 
+    // Called to start the apps execuition
     int OnExecute();
 
+    // Called to initialize the app
     bool OnInit();
+    // Event handler for app
     void OnEvent(SDL_Event* event);
+    // Main loop for execution
     void OnLoop();
+    // Render the current state
     void OnRender();
+    // Cleanup after execution is complete
     void OnCleanup();
 };
 
