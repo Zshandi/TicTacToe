d589747d6f842d78b689e37642f18129280c0655
Zshandi Krahn <ZshandiK@gmail.com>
2020-03-20 10:40:03 -0700

    Moved winCombos to source

--- CApp.cpp
+++ CApp.cpp
@@ -3,6 +3,8 @@
 #undef main
 using namespace std;
 
+const int winCombos[8][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
 int CApp::OnExecute(){
     if(OnInit() == false){
         return -1;
--- CApp.h
+++ CApp.h
@@ -7,8 +7,6 @@
 #include "ComputerPlayer.h"
 #include "RowChecker.h"
 
-int winCombos[8][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-
 class CApp{
 private:
     bool running = true;
